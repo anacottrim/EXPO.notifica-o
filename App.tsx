3e612f18345d05aaa89d6f0510d819cd0206edb8
anacottrim <carolcotrim123@gmail.com>
2024-08-24 11:55:14 -0400

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as Notifications from "expo-notifications";
+import { useEffect, useRef, useState } from "react";
+import "./src/configs/notification";
 
 export default function App() {
+  const notificationListener = useRef<Notifications.Subscription>();
+  const [notification, setNotification] =
+    useState<Notifications.Notification>();
+
+  useEffect(() => {
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    return () => {
+      if (notificationListener.current) {
+        Notifications.removeNotificationSubscription(
+          notificationListener.current
+        );
+      }
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Expo notifications 101!</Text>
+      <Text>Titulo: {notification?.request.content.title}</Text>
+      <Text>Conteúdo: {notification?.request.content.body}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +37,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
